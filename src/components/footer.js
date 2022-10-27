e48854937a27159e8b20d1f6f6ab5ddef7888a41
39otsu <macgyver.codilla39@outlook.jp>
2022-10-27 10:35:25 -0700

    added link

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,11 @@ export default function Footer() {
     <section style={{position: 'fixed', left: '0', bottom: '0', width: '100%'}}>
       <div>
         <div>
-          <Typography variant='subtitle2' sx={{textAlign:'center'}}>
-            {new Date().getFullYear().toString()} | MC
-          </Typography>
+          <Link color='inherit' href='https://github.com/39otsu/language_learning' underline='none'>
+            <Typography variant='subtitle2' sx={{textAlign:'center'}}>
+              {new Date().getFullYear().toString()} | MC
+            </Typography>
+          </Link>
         </div>
       </div>
     </section>
